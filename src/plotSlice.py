63bf1848769e460a3252a02aeb265f3b482a4d57
pnjun <leverfab@gmail.com>
2019-05-23 09:31:45 +0200

    parameters

--- src/plotSlice.py
+++ src/plotSlice.py
@@ -6,8 +6,8 @@ from datetime import datetime
 from attrdict import AttrDict
 
 cfg = {    'data'     : { 'path'     : '/media/Data/Beamtime/processed/',     
-                          'index'    : 'index_calibtest.h5',
-                          'trace'    : 'calibtest.h5' #'71001915-71071776.h5' #'first block.h5' 
+                          'index'    : 'index.h5',
+                          'trace'    : 'first_block.h5' #'71001915-71071776.h5' #'first block.h5' 
                         }
       }
 cfg = AttrDict(cfg)
@@ -16,8 +16,8 @@ cfg = AttrDict(cfg)
 idx = pd.HDFStore(cfg.data.path + cfg.data.index, 'r')
 tr  = pd.HDFStore(cfg.data.path + cfg.data.trace, 'r')
 
-start = datetime(2019,3,25,15,24,50).timestamp()
-stop  = datetime(2019,3,25,15,24,51).timestamp()
+start = datetime(2019,3,25,15,24,30).timestamp()
+stop  = datetime(2019,3,25,15,24,31).timestamp()
 
 pulse = idx.select('pulses', where='time >= start and time < stop')
 
@@ -27,7 +27,6 @@ print(pulse['retarder'])
 data = tr.select('shotsTof', where='pulseId >= pulse.index[0] and pulseId < pulse.index[-1]')
 
 
-#data.mean().plot()
-print(data.iloc[0].index[0], data.iloc[0].index[-1])
+data.mean().plot()
 #plt.plot(np.arange(0,2700)*0.0005,data.mean().values)
-#plt.show()
+plt.show()
