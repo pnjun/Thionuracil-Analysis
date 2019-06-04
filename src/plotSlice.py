5f61cdaefb70544960e6d7ea1a8e1f2f911b0225
pnjun <leverfab@gmail.com>
2019-06-04 13:24:09 +0200

    update plotter

--- src/plotSlice.py
+++ src/plotSlice.py
@@ -18,31 +18,21 @@ cfg = AttrDict(cfg)
 idx = pd.HDFStore(cfg.data.path + cfg.data.index, 'r')
 tr  = pd.HDFStore(cfg.data.path + cfg.data.trace, 'r')
 
-start = datetime(2019,3,25,12,11,0).timestamp()
-stop  = datetime(2019,3,25,12,13,0).timestamp()
+#start = datetime(2019,3,25,12,11,0).timestamp()
+#stop  = datetime(2019,3,25,12,13,0).timestamp()
+
+start = datetime(2019,3,26,2,3,0).timestamp()
+stop  = datetime(2019,3,26,2,5,0).timestamp()
 
 pulse = idx.select('pulses', where='time >= start and time < stop')
 
 print(pulse[['retarder','opisEV']])
+retarder = pulse.retarder.mean()
 
 data = tr.select('shotsTof', where='pulseId >= pulse.index[0] and pulseId < pulse.index[-1]')
 data = data.mean()
 
-
-        
-offset = 7
-conv = evConverter.mainTof(-0.18)
-tof = ( np.arange(350,3350) + offset ) * 0.0005
-evNew = conv(tof)
-
-plt.figure()
-plt.plot(evNew, data)
-plt.plot(data.index, data)
-
-plt.figure()
-plt.plot(tof, data.index)
-plt.plot(tof, evNew)
-
+plt.plot(data.index - retarder, data)
 plt.show()
 
 idx.close()
