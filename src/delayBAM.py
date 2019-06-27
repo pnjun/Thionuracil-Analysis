f04bd5b1494542d686cdf3340ef657002008c185
pnjun <leverfab@gmail.com>
2019-06-27 13:59:48 +0200

    BAM included and i/o done in chunks to circunvent memory problems

--- src/delayBAM.py
+++ src/delayBAM.py
@@ -13,16 +13,16 @@ from utils import filterPulses
 
 cfg = {    'data'     : { 'path'     : '/media/Data/Beamtime/processed/',
                           'index'    : 'index_BAM.h5',
-                          'trace'    : 'first_block_BAM.h5' #'71001915-71071776.h5' #'first block.h5'
+                          'trace'    : 'first_block_BAM.h5'
                         },
-           'time'     : { 'start' : datetime(2019,3,27,0,30,0).timestamp(),
-                          'stop'  : datetime(2019,3,27,1,0,0).timestamp(),
+           'time'     : { 'start' : datetime(2019,3,26,20,59,0).timestamp(),
+                          'stop'  : datetime(2019,3,26,21,53,0).timestamp(),
                         },
            'filters'  : { 'undulatorEV' : (270,271),
                           'retarder'    : (-81,-79),
-                          'waveplate'   : (6,9)
+                          'waveplate'   : (8,11)
                         },
-           'delaybins': 1178.5 + np.arange(-4, 4, 0.01),
+           'delaybins': np.arange(1175,1179,0.01)
       }
 cfg = AttrDict(cfg)
 
@@ -35,11 +35,16 @@ pulsesLims = (pulses.index[0], pulses.index[-1])
 #Filter only pulses with parameters in range
 pulses = filterPulses(pulses, cfg.filters)
 #Get corresponing shots
-shotsData = tr.select('shotsData', where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]', 'pulseId in pulses.index'] )
+if(len(pulses) == 0):
+    print("No pulses satisfy filter condition. Exiting")
+    exit()
+shotsData = tr.select('shotsData', where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]',
+                                          'pulseId in pulses.index'] )
 #Remove pulses with no corresponing shots
 pulses = pulses.drop( pulses.index.difference(shotsData.index.levels[0]) )
 #Remove unpumped pulses
 shotsData = shotsData.query('shotNum % 2 == 0')
+
 
 #Define CUDA kernel for delay adjustment
 @cuda.jit
@@ -51,36 +56,57 @@ delay = cp.array(pulses.delay.values)
 #Shift BAM and add column with new data
 shiftBAM[pulses.shape[0], shotsData.index.levels[1].shape[0] // 2](bam,delay)
 shotsData['delay'] = bam.get()
-'''
-for index, row in pulses.iterrows():
-    shotsData.loc[index, 'delay'] = row.delay
-'''
 
-print(pulses.shape)
-print(shotsData.shape)
+shotsData.delay.hist(bins=100)
+plt.show()
+plt.plot()
 
-#Read in TOF data and calulate difference
-shotsTof  = tr.select('shotsTof',  where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]', 'pulseId in pulses.index'] )
-shotsUvOn   = shotsTof.query('shotNum % 2 == 0')
-shotsUvOff  = shotsTof.query('shotNum % 2 == 1')
-del shotsTof
-shotsDiff = pd.DataFrame( shotsUvOn.to_numpy() - shotsUvOff.to_numpy(), index = shotsUvOn.index )
-del shotsUvOff
+print(f"Loading {shotsData.shape[0]} shots")
 
 #Bin data on delay
 bins = shotsData.groupby( pd.cut( shotsData.delay, cfg.delaybins ) )
 
-#Create output image
-img = []
-delays = []
-for name, group in bins:
-    if len(group):
-        img.append   ( shotsDiff.loc[group.index].mean() )
-        delays.append( name.mid )
+#Gets a TOF dataframe and uses CUDA to calculate pump probe difference specturm
+def getDiff(tofTrace):
+    #Cuda kernel for pump probe difference calculation
+    @cuda.jit
+    def tofDiff(tof):
+        tof[ 2 * cuda.blockIdx.x    , cuda.blockIdx.y*cuda.blockDim.x + cuda.threadIdx.x ] -= \
+        tof[ 2 * cuda.blockIdx.x + 1, cuda.blockIdx.y*cuda.blockDim.x + cuda.threadIdx.x ]
+    #Get difference data
+    tof = cp.array(tofTrace.to_numpy())
+    tofDiff[ (tof.shape[0] // 2 , tof.shape[1] // 250) , 250 ](tof)
+    return pd.DataFrame( tof[::2].get(), index = tofTrace.index[::2], columns=tofTrace.columns)
 
-img = np.array(img)
+#Read in TOF data and calulate difference
+shotsTof  = tr.select('shotsTof',  where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]',
+                                          'pulseId in pulses.index'],
+                                   iterator=True, chunksize=200000)
 
-plt.pcolor(shotsUvOn.iloc[0].index + 80, delays ,img, cmap='bwr')
+#Create empty ouptut image image
+img = np.zeros( ( len(bins), 3000 ))
+binCount = np.zeros( len(bins) )
+#Iterate over data chunks and accumulate them in img
+for counter, chunk in enumerate(shotsTof):
+    print( f"loading chunk {counter}", end='\r' )
+    shotsDiff = getDiff(chunk)
+
+    for binId, bin in enumerate(bins):
+        name, group = bin
+        try:
+            img[binId] += shotsDiff.reindex(group.index).mean()
+            binCount[binId] += 1
+        except KeyError:
+            pass
+
+#average all chunks, counter + 1 is the total number of chunks we loaded
+print()
+print(counter)
+img /= binCount[:,None]
+delays = np.array( [name.mid for name, _ in bins] )
+
+#plot resulting image
+plt.pcolor(shotsDiff.iloc[0].index + 80, delays ,img, cmap='bwr')
 
 plt.figure()
 plt.plot(delays, img.T[830:860].sum(axis=0))
