1e7ec78b52e142da641f7217579d4f6c73ef419e
Dennis <50262273+gveard@users.noreply.github.com>
2019-07-17 17:27:55 +0200

    update

--- src/gmdSort.py
+++ src/gmdSort.py
@@ -1,29 +1,39 @@
-#!/usr/bin/3.6
-'''
-Contains functions to (sort,) bin and normalise TOF signal to FEL pulse energy or UV laser power.
-'''
-from attrdict import AttrDict
-import time
-import datetime
+#!/usr/bin/python3
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from attrdict import AttrDict
 
-cfg = { 'data'   : { 'path'   : '/media/Data/Beamtime/processed/',
-                     'index'  : 'index.h5',
-                     'trace' : 'first_block.h5'},
-        'hdf'    : { 'pulses' : '/pulses',
-                     'time'   : 'time',
-                     'param'    : 'uvPow',
-                     'tof'    : '/shotsTof',
-                     'photon' : '/shotsData'},
-        'time'   : { 'start'  : '26.03.19 04:42:00',
-                     'stop'   : '26.03.19 05:00:00'}}
+from utils import mainTofEvConv
+from utils import filterPulses
+
+from numba import cuda
+import cupy as cp
+
+import matplotlib
+matplotlib.use("GTK3Agg")
+
+cfg = { 'data'    : { 'path'   : '/media/Fast1/ThioUr/processed/',
+                      'index'  : 'index.h5',
+                      'trace'  : 'first_block.h5'},
+        'hdf'     : { 'pulses' : '/pulses',
+                      'time'   : 'time',
+                      'param'  : 'GMD',
+                      'tof'    : '/shotsTof',
+                      'photon' : '/shotsData'},
+        'time'    : { 'start' : datetime(2019,3,25,21,27,0).timestamp(),
+                      'stop'  : datetime(2019,3,25,23,38,0).timestamp()},
+        'filters' : { 'undulatorEV' : (270,271),
+                      'retarder'    : (-81,-79)},
+        'ioChunkSize' : 25000
+      }
 
 cfg = AttrDict(cfg)
 
 
-def laserIntBin(tofData, laserData, bins=10):
-    """Bins ToF traces by GMD values within a macrobunch.
+def binToGmd(tofData, laserData, bins=10):
+    """Bins ToF traces by GMD/UV power values.
 
     Keyword arguments:
     tofData -- standard tof dataframe from hdf5 files
@@ -31,8 +41,7 @@ def laserIntBin(tofData, laserData, bins=10):
     bins -- integer (number of bins) or pandas IntervalIndex object
 
     Output:
-    binnedTof -- pandas Dataframe with multindex (pulseId, bins), ToF traces are summed for each bin
-    binnedLaser -- pandas Dataframe with multindex (pulseId, bins), includes mean and sum for each bin
+    tofBinned -- pandas Dataframe with bins as index, columns remain the same, tof traces are averaged for each bin
     """
     if type(bins) == int:
         # calculate extrema over given dataframe and create interval range
@@ -41,102 +50,136 @@ def laserIntBin(tofData, laserData, bins=10):
         bins = pd.interval_range(start=laserMin, end=laserMax, periods=bins)
     elif type(bins) != pd.core.indexes.interval.IntervalIndex:
         raise TypeError("You should use an integer or pandas IntervalIndex object for bins!")
+    
+    ldBinned = pd.cut(laserData, bins)
+    tofBinned = pd.DataFrame(data=tofData.groupby(ldBinned).mean().to_numpy(), index=bins, columns=tofData.columns)
 
-    print("Binning ToF traces:")
-    i = 0
-    binnedTof = [] # list for storing binned tofs
-    binnedLaserSum = [] # list for storing binned gmds
-    binnedLaserMean = []
-    for bunch in tofData.index.levels[0].values:
-        i += 1
-        print("Bunch: {0:02d}/{1}".format(i, tofData.index.levels[0].values.size), end="\r")
-        # define local variables with data (turns out this is faster than doing it in one line...)
-        pulseTofData = tofData.loc[bunch]
-        pulseLaserData = laserData.loc[bunch]
-        # sort indices from GMD data according to intervals
-        laserBinned = pd.cut(pulseLaserData, bins)
-        # group actual data using sorted indices
-        pulseLaserData = pulseLaserData.groupby(laserBinned)
-        pulseTofData = pulseTofData.groupby(laserBinned)
-        # append to lists
-        for a in pulseTofData.sum().values:
-            binnedTof.append(a)
-        for b in pulseLaserData.sum().values:
-            binnedLaserSum.append(b)
-        for c in pulseLaserData.mean().values:
-            binnedLaserMean.append(c)
+    return tofBinned
 
-    # create new MultiIndex including bunch
-    index = []
-    for i in tofData.index.levels[0].values:
-        for j in bins:
-            index.append([i, j])
-    index = pd.MultiIndex.from_arrays(np.array(index).transpose(), names=["pulseId", "bins"])
 
-    # create new dataframes with binned data
-    binnedTof = pd.DataFrame(data=np.array(binnedTof), index=index)
-    binnedLaser = pd.DataFrame(data=np.array([binnedLaserSum, binnedLaserMean]).transpose(), index=index, columns=["Sum", "Mean"])
-    print("")
-    print("Done. Returning binned data.")
-
-    return binnedTof, binnedLaser
-
-def laserIntNorm(tofData, laserData):
-    """Divide each ToF trace of a pulse within a macrobunch by its GMD value and returns the normalised tof dataframe.
+def normToGmd(tofTrace, gmd):
+    """Divide each ToF trace by its xray or uv power value and returns the normalised tof dataframe.
 
     Keyword arguments:
     tofData -- standard tof dataframe from hdf5 files
-    laserData -- standard gmd dataframe from hdf5 files
+    gmd -- standard gmd dataframe from hdf5 files
 
     Output:
-    normedTof -- dataframe similar to tofData but normalised on GMD data and no column names
+    Normalised tofTraces
     """
-    print("Normalize Tof traces:")
-    i = 0
-    normedTof = []
-    for bunch in tofData.index.levels[0].values: # loop over bunches
-        i += 1
-        for pulse in tofData.index.levels[1].values: # loop over pulses
-            print("Bunch: {0:02d}/{1}, Pulse: {2:02d}/50".format(i, tofData.index.levels[0].values.size, pulse+1), end="\r")
-            # define local variables with data (turns out this is faster than doing it in one line...)
-            pulseTofData = tofData.loc[bunch].loc[pulse]
-            pulselaserData = laserData.loc[bunch].loc[pulse]
-            # divide ToF data by GMD
-            pulseTofData = pulseTofData / pulselaserData
-            # store new ToF in list
-            normedTof.append(pulseTofData.values)
+    @cuda.jit
+    def normTofToGmd(tof, gmd):
+        row = 2 * cuda.blockIdx.x
+        col = cuda.blockIdx.y*cuda.blockDim.x + cuda.threadIdx.x
+        tof[ row    , col ]  /= gmd[row]
 
-    print("")
-    # Create new dataframe
-    tofData = pd.DataFrame(data = np.array(normedTof), index=tofData.index)
-    print("Done. Returning normalized dataframe.")
-    # return stuff
-    return tofData
+    #Get difference data
+    tof = cp.array(tofTrace.to_numpy())
+    cuGmd = cp.array(gmd.reindex(tofTrace.index).to_numpy())
+    normTofToGmd[ (tof.shape[0] // 2 , tof.shape[1] // 250) , 250 ](tof, cuGmd)
+
+    return pd.DataFrame( tof.get(), index = tofTrace.index, columns=tofTrace.columns)
 
 
-def laserIntSort(tofData, laserData):
-    """Sorts ToF traces by GMD values within a macrobunch.
+def do_sum_stuff(data, indices, lim=None, fit=1):
 
-    Keyword arguments:
-    tofData -- standard tof dataframe from hdf5 files
-    laserData -- standard gmd dataframe from hdf5 files
-    """
-    pass
+    s = np.array([i.sum() for i in data.to_numpy()[:,indices[0]:indices[1]]]) / (data.columns.values[indices[1]] - data.columns.values[indices[0]])
+    r = pd.DataFrame(data=s, index=data.index.values)
+    #if lim:
+        #ind = r.where(r.index.to_series() < lim).dropna().index
+        #p = np.polyfit(ind, r.loc[ind], 1)#, cov=True)
+        #c = np.sqrt(np.diag(np.array([[i[0] for i in j] for j in c])))
+    #else:
+        #p = np.polyfit(r.index, r, 1)#, cov=True)
+        #c = np.sqrt(np.diag(np.array([[i[0] for i in j] for j in c])))
+
+    return r#, p, ind
 
 
 if __name__ == '__main__':
 
-    start = int(time.mktime(datetime.datetime.strptime(cfg.time.start, '%d.%m.%y %H:%M:%S').timetuple()))
-    stop  = int(time.mktime(datetime.datetime.strptime(cfg.time.stop, '%d.%m.%y %H:%M:%S').timetuple()))
-
     idx = pd.HDFStore(cfg.data.path + cfg.data.index, 'r')
     tr  = pd.HDFStore(cfg.data.path + cfg.data.trace, 'r')
-    pulse = idx.select('pulses', where='time >= start and time < stop')
-    param = tr.select('shotsData', where='pulseId >= pulse.index[0] and pulseId <= pulse.index[-1]')[cfg.hdf.param]
-    data = tr.select('shotsTof', where='pulseId >= pulse.index[0] and pulseId <= pulse.index[-1]')
+    pulses = idx.select('pulses', where='time >= cfg.time.start and time < cfg.time.stop')
+    pulsesLims = (pulses.index[0], pulses.index[-1])
+    #Filter only pulses with parameters in range
+    pulses = filterPulses(pulses, cfg.filters)
     idx.close()
+
+    #Get corresponing shots
+    if(len(pulses) == 0):
+        print("No pulses satisfy filter condition. Exiting\n")
+        exit()
+    shotsData = tr.select('shotsData', where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]', 'pulseId in pulses.index'] )[cfg.hdf.param]
+    #Remove pulses with no corresponing shots
+    pulses = pulses.drop( pulses.index.difference(shotsData.index.levels[0]) )
+    
+    # some analysis on gmd
+    print("do some stuff with GMD data.", end="\r")    
+    shotsMean = np.array([shotsData.loc[bunch].mean() for bunch in shotsData.index.levels[0].values])
+
+    f1, ax1 = plt.subplots(1,3, figsize=(12,4))
+    ax1[0].plot(shotsMean,lw=1)
+    shotsData.hist(bins=125, ax=ax1[1]) 
+
+    shotsBunch = np.array([shotsData.loc[:, pulse].mean() for pulse in range(50)])
+    ax1[2].plot(np.linspace(1,51, 50), shotsBunch)
+    plt.tight_layout()
+    print("                               ", end="\r")
+    print("done with gmd stuff.", end="\r")
+    
+    # go for tof data
+    intervals = pd.interval_range(start=0, end=125, freq=1.)
+    shotsTof  = tr.select('shotsTof', where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]',
+                                             'pulseId in pulses.index'],
+                                      iterator=True, chunksize=cfg.ioChunkSize)
+
+    img = np.zeros( ( len(intervals), 3000 ))
+    binCount = np.zeros( len(intervals) )
+    for counter, chunk in enumerate(shotsTof):
+        c = 0
+        print( f"processing chunk {counter}", end='\r' )
+        idx = chunk.index.levels[0].values
+        #shotsnorm = gmdNorm(chunk, gmd.loc[idx])
+        shotsBins = pd.cut(shotsData.loc[idx], intervals)
+        tofs = pd.DataFrame(chunk.groupby(shotsBins).mean().to_numpy())
+        for group in range(tofs.index.size):
+            if not tofs.loc[group].isnull().to_numpy().any():
+                img[c] += tofs.loc[group].to_numpy()
+                binCount[c] += 1
+            c += 1
+
+    img /= binCount[:,None]
+    bg = np.array([np.mean(i[-50:-1]) for i in img])
+    img -= bg[:,None]
+    
+    gmdBins = shotsData.groupby(pd.cut(shotsData, intervals)).mean()
+    tofs = tr.select("shotsTof", where=["pulseId == pulsesLims[0]", "pulseId in pulses.index"]).columns
     tr.close()
 
-    #data = laserIntNorm(data,param)
-    intervals = pd.interval_range(start=10, end=60, periods=10)
-    data, param = laserIntBin(data, param, bins=intervals)
+    evConv = mainTofEvConv(pulses.retarder.mean())
+    evs = evConv(tofs.to_numpy(dtype=np.float64))
+
+    img = pd.DataFrame(data=img, index=gmdBins.values, columns=evs, dtype="float64").dropna()
+
+    r1 = do_sum_stuff(img, [70,350], lim=60.)
+    r2 = do_sum_stuff(img, [360,780], lim=50.)
+    r3 = do_sum_stuff(img, [790,1100], lim=40.)
+    f2, ax2 = plt.subplots(1,2, figsize=(9,4))
+
+    ax2[0].pcolormesh(img.columns.values, img.index.values,img, cmap="bwr", vmax=30, vmin=-30)
+    ax2[0].set_xlabel("kinetic energy (ev)")
+    ax2[0].set_ylabel("GMD (µJ)")
+    ax2[1].plot(r1.index.values, r1*15, "b.", label="1 (x15)")
+    ax2[1].plot(r2.index.values, r2*2.5, "r.", label="2 (x2.5)")
+    ax2[1].plot(r3.index.values, r3, "g.", label="3")
+    #ax2[1].plot(ind1, np.polyval(p1, ind1), "b-")
+    #ax2[1].plot(ind2, np.polyval(p2, ind2), "r-")
+    #ax2[1].plot(ind3, np.polyval(p3, ind3), "g-")
+
+    ax2[1].set_xlabel("GMD (µJ)")
+    ax2[1].set_ylabel("Average signal (arb.units)")
+    ax2[1].legend()
+
+    plt.tight_layout()
+    plt.show()
