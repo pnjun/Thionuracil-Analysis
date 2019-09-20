aea27d2634ad80c1f41b1823d0d41a7bc6010cbd
Dennis <50262273+gveard@users.noreply.github.com>
2019-09-20 14:18:30 +0200

    script distinguishes between pumped and unpumped shots

--- src/nexafs.py
+++ src/nexafs.py
@@ -18,17 +18,17 @@ cfg = {    'data'     : { 'path'     : '/media/Fast1/ThioUr/processed/',
                           'index'    : 'index.h5',
                           'trace'    : 'third_block.h5'
                         },
-           'time'     : { 'start' : datetime(2019,4,4,21,51,0).timestamp(),
-                          'stop'   : datetime(2019,4,4,22,25,0).timestamp()
+           'time'     : { 'start' : datetime(2019,4,6,16,2,0).timestamp(),
+                          'stop'  : datetime(2019,4,6,18,50,0).timestamp()
                         },
-           'filters'  : { 'undulatorEV' : (150,180), # replace with opisEV if opis column contains reasonable values, else use undulatorEV
-                          #'delay'       : (1260.6,1260.8), # comment out if there is only non-UV data in the block
+           'filters'  : { 'undulatorEV' : (160,180), # replace with opisEV if opis column contains reasonable values, else use undulatorEV
+                          'delay'       : (1261.35,1261.45), # comment out if there is only non-UV data in the block
                           'retarder'    : (-11,-9)
                         },
            'output'   : { 'img'  : './Plots/',  # where output images should be stored
                           'data' : './data/'    # where processed data should be stored
                         },
-           'UV'       : True,
+           'UV'       : True,  # True, if you want to distinguish between even (pumped) and odd (unpumped) shots in a macrobunch
            'ROI'      : "all",  # eV region to be summed, either tuple or all
            'PLC'      : False,  # photoline correction in region of interest
            'evStep'   : 1.5,  # frequency for binning
@@ -37,8 +37,8 @@ cfg = {    'data'     : { 'path'     : '/media/Fast1/ThioUr/processed/',
 
 cfg = AttrDict(cfg)
 
-timetag = '{0}-{1}'.format(datetime.fromtimestamp(cfg.time.start).isoformat(),
-                           datetime.fromtimestamp(cfg.time.stop).isoformat())
+timetag = "S2p_+200fs_02"#'{0}-{1}'.format(datetime.fromtimestamp(cfg.time.start).isoformat(),
+          #                 datetime.fromtimestamp(cfg.time.stop).isoformat())
 
 idx = pd.HDFStore(cfg.data.path + cfg.data.index, 'r')
 tr  = pd.HDFStore(cfg.data.path + cfg.data.trace, 'r')
@@ -52,19 +52,20 @@ assert len(pulses) != 0, "No pulses satisfy filter condition"
 
 idx.close()
 
+# Get GMD data for shots
 shotsData = tr.select('shotsData', where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]',
                                           'pulseId in pulses.index'] )
 
-if cfg.UV:
-    shotsData = shotsData.query('shotNum % 2 == 1')
+shotsData = shotsData.query("GMD > .01")
+gmdWUV = shotsData.query("shotNum % 2 == 0").pivot_table(index="pulseId", columns="shotNum", values="GMD")
+gmdNUV = shotsData.query("shotNum % 2 == 1").pivot_table(index="pulseId", columns="shotNum", values="GMD")
 
-shotsData = shotsData.pivot_table(index="pulseId", columns="shotNum", values="GMD")
-shotsData = shotsData.where(shotsData >= .01).dropna() # remove data where fel was off
-
-#Remove pulses with no corresponing shots
-pulses = pulses.drop( pulses.index.difference(shotsData.index) )
+# Remove pulses with no corresponing shots
+pulses = pulses.drop( pulses.index.difference(gmdNUV.index) )
 pulsesLims = (pulses.index[0], pulses.index[-1])
 
+
+# check whether OPIS or Undulator values are used
 evLim = None
 photEn = None
 try:
@@ -75,109 +76,112 @@ except AttributeError:
     photEn = pulses.undulatorEV
 
 # bin pulse energies to photon energy
-intervals = pd.interval_range(start=evLim[0], end=evLim[1], freq=cfg.evStep)
-#left = [161, 162.5, 163.5, 164.5, 165.25, 166, 166.75, 167.5, 169.0, 170.5, 172]
-#right = [162, 163.5, 164.5, 165, 165.75, 166.5, 167.25, 168.5, 170, 171.5, 173]
-#intervals = pd.IntervalIndex.from_arrays(left, right)
+# first, create intervals
+# for equally spaced energy steps use the first intervals variable and comment out the following 3 lines
+# for weirdly spaced energy steps use the other three lines and define interval borders with left and right
+#intervals = pd.interval_range(start=evLim[0], end=evLim[1], freq=cfg.evStep)
+left = [161, 162.5, 163.5, 164.5, 165.25, 166, 166.75, 167.5, 169.0, 170.5, 172]
+right = [162, 163.5, 164.5, 165, 165.75, 166.5, 167.25, 168.5, 170, 171.5, 173]
+intervals = pd.IntervalIndex.from_arrays(left, right)
 evBin = pd.cut(photEn, intervals)
-gmdBin = shotsData.mean(axis=1).groupby(evBin)
-gmdStat = gmdBin.agg(["mean", "std", "count"])
-gmdBin = gmdBin.mean()
+gmdBinWUV = gmdWUV.mean(axis=1).groupby(evBin).agg(["sum", "mean", "std", "count"])
+gmdBinNUV = gmdNUV.mean(axis=1).groupby(evBin).agg(["sum", "mean", "std", "count"])
 opisBin = photEn.groupby(evBin).mean()
-
-# plot number of bunches per bin and average gmd per bin
-f1, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-ax1.plot(opisBin, gmdStat["count"] / 1000, ".")
-ax1.set_ylabel("Number of MBs (x1000)")
-ax2.errorbar(opisBin.dropna().values, gmdStat["mean"].dropna().values, fmt=".", yerr=gmdStat["std"].dropna().values)
-ax2.set_xlabel("Photon Energy (eV)")
-ax2.set_ylabel("Mean GMD value")
-plt.tight_layout()
-plt.savefig(cfg.output.img + "nexafs_{0}_stats.png".format(timetag), dpi=300)
 
 # look at actual tof data
 shotsTof  = tr.select('shotsTof',
                       where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]', 'pulseId in pulses.index'],
                       iterator=True,
                       chunksize=cfg.ioChunkSize)
-img = np.zeros( ( len(intervals), 3000 ))
-gmd = np.zeros( len(intervals) )
+
+imgWUV = np.zeros( ( len(intervals), 3000 ))
+imgNUV = np.zeros( ( len(intervals), 3000 ))
 binCount = np.zeros( len(intervals) )
 for counter, chunk in enumerate(shotsTof):
     c = 0
     print( f"processing chunk {counter}", end='\r' )
     idx = chunk.index.levels[0].values
-    #chunk = normToGmd(chunk, shotsData.loc[idx])
-    chunk = chunk.groupby(level=[0]).mean()
-    gmds = shotsData.loc[idx].mean(axis=1)
+    chunkWUV = chunk.query("shotNum % 2 == 0").groupby(level=[0]).mean()
+    chunkNUV = chunk.query("shotNum % 2 == 1").groupby(level=[0]).mean()
     shotsBins = pd.cut(photEn.loc[idx], intervals)
-    tofs = pd.DataFrame(chunk.groupby(shotsBins).mean().to_numpy())
-    gmds = pd.DataFrame(gmds.groupby(shotsBins).mean().to_numpy())
-    #print(gmds)
-    for group in range(tofs.index.size):
-        if not tofs.loc[group].isnull().to_numpy().any():
-            img[c] += tofs.loc[group].to_numpy()
-            gmd[c] += gmds.loc[group].to_numpy()
+    tofsWUV = pd.DataFrame(chunkWUV.groupby(shotsBins).mean().to_numpy())
+    tofsNUV = pd.DataFrame(chunkNUV.groupby(shotsBins).mean().to_numpy())
+    for group in range(tofsWUV.index.size):
+        if not tofsWUV.loc[group].isnull().to_numpy().any():
+            imgWUV[c] += tofsWUV.loc[group].to_numpy()
+            imgNUV[c] += tofsNUV.loc[group].to_numpy()
             binCount[c] += 1
         c += 1
 
 tofs = tr.select("shotsTof", where=["pulseId == pulsesLims[0]", "pulseId in pulses.index"]).columns
 tr.close()
 
-img /= binCount[:,None]
-gmd /= binCount
-img /= gmd[:,None]
+imgWUV /= binCount[:,None] # average energy binned tof traces with number of used chunks
+imgWUV /= gmdBinWUV["mean"][:,None]  # normalise average tof traces on average gmd
+imgNUV /= binCount[:,None]
+imgNUV /= gmdBinNUV["mean"][:,None]
 
-bg = np.array([ j[:100].mean() for j in img])
-img -= bg[:,None]
-
+# calulate eVs
 evConv = mainTofEvConv(pulses.retarder.mean())
 evs = evConv(tofs.to_numpy(dtype=np.float64))
 
 # make proper pandas dataframe
-img = pd.DataFrame(data=img, index=opisBin.values, columns=evs, dtype="float64").dropna()
+imgWUV = pd.DataFrame(data=imgWUV, index=opisBin.values, columns=evs, dtype="float64").dropna()
+imgNUV = pd.DataFrame(data=imgNUV, index=opisBin.values, columns=evs, dtype="float64").dropna()
 
 # save this dataframe to text file
-img.to_csv(cfg.output.data + "nexafs_{0}.csv".format(timetag), header=True, index=True , mode="w")
+imgWUV.to_csv(cfg.output.data + "nexafs_{0}_wuv.csv".format(timetag), header=True, index=True , mode="w")
+imgNUV.to_csv(cfg.output.data + "nexafs_{0}_nuv.csv".format(timetag), header=True, index=True , mode="w")
 
-#img = pd.read_csv(cfg.output.data + "nexafs_{0}.csv".format(timetag), header=0, index_col=0)
-#img.columns = img.columns.astype('float64')
-d = np.array([i - i.max() for i in img.values])
-img = pd.DataFrame(data=d, columns=img.columns, index=img.index)
+# preparing stuff for plotting
+# changing the offset so that data has same sign everywhere
 
-# make a plot
-f2, ax = plt.subplots(1, 2, sharey=True, figsize=(10,4), gridspec_kw={'width_ratios': [2, 3]})
-cm = ax[1].pcolormesh(img.columns.values, img.index.values, img.values, cmap="cividis")
-cb = plt.colorbar(cm)
-ax[1].set_xlabel("Kinetic Energy (eV)")
-cb.set_label("GMD normalised average Counts")
+d = np.array([i - i.max() for i in imgWUV.values])
+imgWUV = pd.DataFrame(data=d, columns=imgWUV.columns, index=imgWUV.index)
+d = np.array([i - i.max() for i in imgNUV.values])
+imgNUV = pd.DataFrame(data=d, columns=imgNUV.columns, index=imgNUV.index)
+del d
 
-# look at region of interest
+def plot_stuff(dataframe, roi, outfolder, tag, plc=False):
 
-if cfg.ROI == "all":
-    roi = img
-    ax[0].plot(roi.T.sum()/1000, roi.index, ".")
-    ax[0].set_xlabel('Summed Counts (x1000)')
-else:
-    mask = (img == img) & (img.columns >= cfg.ROI[0]) & (img.columns <= cfg.ROI[1])
-    if cfg.PLC:
-        roi = img[mask]
-        roi_max = [ i.argmin() for i in img.values]
-        roi_pl = [ img.values[i][roi_max[i]-25:roi_max[i]+25].sum() for i in range(len(img.values))]
-        roi = roi.T.dropna().T
-        ax[0].plot(roi.T.sum()/1000, roi.index, ".", label="Raw sum")
-        ax[0].plot((roi.T.sum() - roi_pl)/1000, roi.index, ".", label="PL corrected")
-        ax[0].legend(loc=9)
+    f, ax = plt.subplots(1, 2, sharey=True, figsize=(10,4), gridspec_kw={"width_ratios":[2, 3]})
+
+    cm = ax[1].pcolormesh(dataframe.columns.values, dataframe.index.values, dataframe.values, cmap="cividis")
+    cb = plt.colorbar(cm)
+    ax[1].set_xlabel("Kinetic Energy (eV)")
+    cb.set_label("GMD Normalised Signal")
+
+    if roi == "all":
+        ax[0].plot(dataframe.T.sum()/1000, dataframe.index, ".")
+        ax[0].set_xlabel('Summed Counts (x1000)')
     else:
-        roi = img[mask].T.dropna().T
-        ax[0].plot(roi.T.sum()/1000, roi.index, ".", label="Raw sum")
+        mask = (dataframe == dataframe) & (dataframe.columns >= roi[0]) & (dataframe.columns <= roi[1])
+        if plc:
+            d = dataframe[mask]
+            d_max = [ i.argmin() for i in dataframe.values]
+            d_pl = [ dataframe.values[i][d_max[i]-25:d_max[i]+25].sum() for i in range(len(dataframe.values))]
+            d = d.T.dropna().T
+            ax[0].plot(d.T.sum()/1000, d.index, ".", label="Raw sum")
+            ax[0].plot((d.T.sum() - d_pl)/1000, roi.index, ".", label="PL corrected")
+            ax[0].legend(loc=9)
+        else:
+            d = dataframe[mask].T.dropna().T
+            ax[0].plot(d.T.sum()/1000, d.index, ".", label="Raw sum")
 
-    ax[0].set_xlabel('Summed Counts over ROI {0}-{1} eV (x1000)'.format(cfg.ROI[0], cfg.ROI[1]))
+        ax[0].set_xlabel('Summed Counts over ROI {0}-{1} eV (x1000)'.format(roi[0], roi[1]))
 
-ax[0].set_ylabel('Photon Energy (eV)')
+    ax[0].set_ylabel('Photon Energy (eV)')
 
-#ax[0].set_ylim(202,246)
-plt.tight_layout()
-plt.savefig(cfg.output.img + "nexafs_{0}.png".format(timetag), dpi=300)
-#plt.show()
-plt.close("all")
+    plt.tight_layout()
+    plt.savefig(outfolder + "nexafs_{0}.png".format(tag), dpi=300)
+    #plt.show()
+    plt.close("all")
+
+
+plot_stuff(imgWUV, cfg.ROI, cfg.output.img, timetag+"_wuv")
+plot_stuff(imgNUV, cfg.ROI, cfg.output.img, timetag+"_nuv")
+aver = pd.DataFrame((imgWUV.values + imgNUV.values) / 2, index=imgWUV.index, columns=imgWUV.columns)
+plot_stuff(aver, cfg.ROI, cfg.output.img, timetag+"_aver")
+diff = pd.DataFrame(imgWUV.values - imgNUV.values, index=imgWUV.index, columns=imgWUV.columns)
+plot_stuff(diff, cfg.ROI, cfg.output.img, timetag+"_diff")
+
