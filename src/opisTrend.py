4c69543ff76f19b96ac251b47bae8320219b05b0
pnjun <leverfab@gmail.com>
2019-12-04 15:33:31 +0100

    opisTrend now with fwhm trend

--- src/opisTrend.py
+++ src/opisTrend.py
@@ -16,7 +16,7 @@ cfg = {    'data'     : { 'path'     : '/media/Fast1/ThioUr/processed/',
            'time'     : { #'start' : datetime(2019,3,31,6,54,0).timestamp(),
                           #'stop'  : datetime(2019,3,31,6,56,0).timestamp(),
                           'start' : datetime(2019,4,1,2,51,0).timestamp(),
-                          'stop'  : datetime(2019,4,1,2,51,50).timestamp(),
+                          'stop'  : datetime(2019,4,1,2,55,0).timestamp(),
                         },
       }
 
@@ -33,15 +33,23 @@ tof = h5data.select('tof1',
                      where=['pulseId >= pulsesLims[0] and pulseId < pulsesLims[1]',
                             'pulseId in pulses.index'] )
 tof = tof.mean(level=1)
-
 evConv = ou.evConv()
-integ =  []
-fwhm = []
 
 maxIdx = tof.idxmin(axis=1)
-maxima = evConv[CH](maxIdx)
+maxVal = tof.min(axis=1)
+maxEv = evConv[CH](maxIdx)
 integ  = tof.sum(axis=1)
 
+def getFWHM(row):
+    a =  np.where(np.diff(np.signbit(row)))[0]
+    return [a[0], a[1]]
+
+
+#FWHM calulation
+boundsIdx = tof.sub( maxVal/2, axis=0 ).apply(getFWHM, axis=1, raw=True)
+boundsTof = [ tof.columns[bounds] for bounds in boundsIdx ]
+boundsEv  = [ evConv[CH](bTof) for bTof in boundsTof ]
+fwhm = [ bEv[0] - bEv[1] for bEv in boundsEv ]
 
 for n, trace in tof.iterrows():
     if n % 5 == 0:
@@ -50,15 +58,19 @@ for n, trace in tof.iterrows():
 def fit(x, m, q):
     return x*m + q
 
-maxima = np.array(maxima)
-maxima += 15.8 # Photon energy = electron energy + bounding energy
-x = np.array(range(len(maxima)))
-popt, pconv = curve_fit(fit, x, maxima)
+maxEv = np.array(maxEv)
+maxEv += 15.8 # Photon energy = electron energy + bounding energy
+x = np.array(range(len(maxEv)))
+popt, pconv = curve_fit(fit, x, maxEv)
 
-plt.figure()
-plt.plot(x, maxima, 'o')
+plt.figure('energy')
+plt.plot(x, maxEv, 'o')
 plt.plot(x, fit(x, *popt))
 print(popt)
-plt.figure()
-plt.plot(x, integ)
+plt.figure('fwhm')
+plt.plot(x, fwhm)
+plt.figure('scatter')
+plt.plot(maxEv, fwhm, 'o')
+#plt.figure()
+#plt.plot(x, integ)
 plt.show()
