7e4c055e249b85739f98fa5c9ff1c69b6c10052b
pnjun <leverfab@gmail.com>
2020-01-21 16:52:06 +0100

    delayscan now does fragmentsearch

--- src/delayScan.py
+++ src/delayScan.py
@@ -12,23 +12,23 @@ import pickle
 
 cfg = {    'data'     : { 'path'     : '/media/Fast2/ThioUr/processed/',
                           'index'    : 'index.h5',
-                          'trace'    : 'third_block.h5'
+                          'trace'    : 'second_block.h5'
                         },
            'output'   : { 'path'     : './data/',
                           'fname'    : 'stronFieldFragments.csv'
                         },
-           'time'     : { 'start' : datetime(2019,4,5,12,59,0).timestamp(),
-                          'stop'  : datetime(2019,4,5,13,20,0).timestamp(),
+           'time'     : { 'start' : datetime(2019,3,31,11,30,0).timestamp(),
+                          'stop'  : datetime(2019,3,31,11,50,0).timestamp(),
                         },
-           'filters'  : { 'undulatorEV' : (210.,225.),
-                          'retarder'    : (-15.,-0.),
+           'filters'  : { 'undulatorEV' : (205.,250.),
+                          'retarder'    : (-10.,10),
                           #'delay'       : (1170, 1185.0),
-                          'waveplate'   : (0,45)
+                          'waveplate'   : (40,50)
                         },
            'sdfilter' : "GMD > 0.5 & BAM != 0", # filter for shotsdata parameters used in query method
            'delayBin_mode'  : 'QUANTILE', # Binning mode, must be one of CUSTOM, QUANTILE, CONSTANT
-           'delayBinStep'   : 0.01,     # Size of bins, only relevant when delayBin_mode is CONSTANT
-           'delayBinNum'    : 30,     # Number if bis to use, only relevant when delayBin_mode is QUANTILE
+           'delayBinStep'   : 0.1,     # Size of bins, only relevant when delayBin_mode is CONSTANT
+           'delayBinNum'    : 15,     # Number if bis to use, only relevant when delayBin_mode is QUANTILE
            'ioChunkSize' : 50000,
            'gmdNormalize': True,
            'useBAM'      : True,
@@ -37,7 +37,8 @@ cfg = {    'data'     : { 'path'     : '/media/Fast2/ThioUr/processed/',
                        'photoShift' : False,
                        'valence'    : False,
                        'fragmentSearch' : True, #Plot Auger trace at long delays to look for fragmentation
-           }
+           },
+           'writeOutput' : True #Set to true to write out data in csv
       }
 
 
@@ -49,11 +50,16 @@ tr  = pd.HDFStore(cfg.data.path + cfg.data.trace, mode = 'r')
 #Get all pulses within time limits
 pulses = idx.select('pulses', where='time >= cfg.time.start and time < cfg.time.stop')
 #Filter only pulses with parameters in range
-pulses = utils.filterPulses(pulses, cfg.filters)
+fpulses = utils.filterPulses(pulses, cfg.filters)
 
 #Get corresponing shots
-if(not len(pulses)):
+if(not len(fpulses)):
+    print(f"Avg Retarder  {pulses.retarder.mean()}")
+    print(f"Avg Undulator {pulses.undulatorEV.mean()}")
+    print(f"Avg Waveplate {pulses.waveplate.mean()}")
     raise Exception("No pulses satisfy filter condition")
+else:
+    pulses = fpulses
 
 shotsData = utils.h5load('shotsData', tr, pulses)
 
@@ -120,10 +126,14 @@ else:
 shotsTof  = utils.h5load('shotsTof', tr, pulses, chunk=cfg.ioChunkSize)
 
 #Create empty ouptut image image
-img = np.zeros( ( len(bins), 3009 ))
+diffAcc = np.zeros( ( len(bins), 3009 ))
 binCount = np.zeros( len(bins) )
 
-#Iterate over data chunks and accumulate them in img
+if cfg.plots.fragmentSearch:
+    evenAcc = np.zeros( ( len(bins), 3009 ))
+    oddAcc  = np.zeros( ( len(bins), 3009 ))
+
+#Iterate over data chunks and accumulate them in diffAcc
 for counter, chunk in enumerate(shotsTof):
     print( f"loading chunk {counter}", end='\r' )
     shotsDiff = utils.getDiff(chunk, gmdData)
@@ -131,15 +141,25 @@ for counter, chunk in enumerate(shotsTof):
         name, group = bin
         group = group.query(cfg.sdfilter)
         binTrace = shotsDiff.reindex(group.index).mean()
+
         if not binTrace.isnull().to_numpy().any():
-            img[binId] += binTrace
+            if cfg.plots.fragmentSearch:
+                even = chunk.query('shotNum % 2 == 0')
+                odd =  chunk.query('shotNum % 1 == 0')
+                evenAcc[binId] += even.reindex(group.index).mean()
+                oddAcc[binId]  +=  odd.reindex(group.index).mean()
+            diffAcc[binId] += binTrace
             binCount[binId] += 1
 
 idx.close()
 tr.close()
 
-img /= binCount[:,None]
-if uvEven > uvOdd: img *= -1
+diffAcc /= binCount[:,None]
+if cfg.plots.fragmentSearch:
+    evenAcc /= binCount[:,None]
+    oddAcc /= binCount[:,None]
+
+if uvEven > uvOdd: diffAcc *= -1
 
 #get axis labels
 delays = np.array( [name.mid for name, _ in bins] )
@@ -149,14 +169,15 @@ evConv = utils.mainTofEvConv(pulses.retarder.mean())
 evs = evConv(shotsDiff.iloc[0].index.to_numpy(dtype=np.float32))
 
 # make dataframe and save data
-img = pd.DataFrame(data = img, columns=evs, index=delays).fillna(0)
-img.to_csv(cfg.output.path + cfg.output.fname + ".csv", mode="w")
+if cfg.writeOutput:
+    df = pd.DataFrame(data = diffAcc,columns=evs, index=delays).fillna(0)
+    df.to_csv(cfg.output.path + cfg.output.fname + ".csv", mode="w")
 
 #plot resulting image
 if cfg.plots.delay2d:
     plt.figure()
-    cmax = np.max(np.abs(img.values))
-    plt.pcolormesh(evs, delays ,img.values, cmap='bwr', vmax=cmax, vmin=-cmax)
+    cmax = np.max(np.abs(diffAcc))
+    plt.pcolormesh(evs, delays ,diffAcc, cmap='bwr', vmax=cmax, vmin=-cmax)
     plt.xlabel("Kinetic energy (eV)")
     plt.ylabel("Averaged Signal (counts)")
     plt.tight_layout()
@@ -171,31 +192,36 @@ if cfg.plots.photoShift:
     photoline2 = slice(np.abs(evs - 100).argmin() , np.abs(evs - 98.5).argmin())
     photoline3 = slice(np.abs(evs - 107).argmin() , np.abs(evs - 102.5).argmin())
 
-    NegPhLine, = plt.plot(delays, img.values.T[photoline3].sum(axis=0), label = 'negative photoline shift')
-    PosPhLine1, = plt.plot(delays, img.values.T[photoline1].sum(axis=0), label = 'positive photoline shift 90-99 eV')
-    PosPhLine2, = plt.plot(delays, img.values.T[photoline2].sum(axis=0), label = 'positive photoline shift 99-100 eV')
+    NegPhLine, = plt.plot(delays, diffAcc.T[photoline3].sum(axis=0), label = 'negative photoline shift')
+    PosPhLine1, = plt.plot(delays, diffAcc.T[photoline1].sum(axis=0), label = 'positive photoline shift 90-99 eV')
+    PosPhLine2, = plt.plot(delays, diffAcc.T[photoline2].sum(axis=0), label = 'positive photoline shift 99-100 eV')
     plt.legend(handles=[PosPhLine1, PosPhLine2, NegPhLine])
 
 if cfg.plots.valence:
     plt.figure()
     valence = slice( np.abs(evs - 145).argmin() , np.abs(evs - 140).argmin())
-    plt.plot(delays, img.values.T[valence].sum(axis=0))
+    plt.plot(delays, diffAcc.T[valence].sum(axis=0))
 
 if cfg.plots.fragmentSearch:
-    plt.figure()
-    plt.plot(img.values[-1])
+    from matplotlib.widgets import Slider
+    f = plt.figure()
+
+    diffPlot, = plt.plot(evs,diffAcc[-1], label='diffs')
+    evenPlot, = plt.plot(evs,evenAcc[-1], label='even')
+    oddPlot,  = plt.plot(evs,oddAcc[-1], label='odd')
+    #plt.plot(evs,diffAcc.values[0])
+    f.suptitle(delays[-1])
+    def update(val):
+        n = int(val)
+        diffPlot.set_ydata(diffAcc[n])
+        evenPlot.set_ydata(evenAcc[n])
+        oddPlot.set_ydata(oddAcc[n])
+
+        f.suptitle(delays[n])
+        f.canvas.draw_idle()
+
+    slax = plt.axes([0.1, 0.03, 0.65, 0.04])
+    slider = Slider(slax, 'Amp', 0, diffAcc.shape[0]-1, valinit=0)
+    slider.on_changed(update)
 
 plt.show()
-
-'''
-results = np.full((img.shape[0]+1,img.shape[1]+1), np.nan)
-results[1:,1:] = img
-results[1:,0] = delays
-results[0,1:] = evs
-
-
-with open(cfg.outFname + '.pickle', 'wb') as fout:
-    pickle.dump(cfg, fout)
-np.savetxt(cfg.outFname + '.txt', results, header='first row: kinetic energies, first column: delays')#img)
-
-np.savetxt(cfg.outFname + '.txt', results, header='first row: kinetic energies, first column: delays')#img)'''
